d34f5d545e4f628ee13cedab26de0f904a86bea3
Shady <ahmed.shady1@vodafone.com>
2023-08-24 23:02:19 +0300

    feat: adding gson lib to make to add @SerializedName to params

--- domain/src/main/java/com/shady/domain/entity/Owner.kt
+++ domain/src/main/java/com/shady/domain/entity/Owner.kt
@@ -1,22 +1,10 @@
 package com.shady.domain.entity
 
+import com.google.gson.annotations.SerializedName
+
 data class Owner(
-    val avatar_url: String,
-    val events_url: String,
-    val followers_url: String,
-    val following_url: String,
-    val gists_url: String,
-    val gravatar_id: String,
-    val html_url: String,
-    val id: Int,
-    val login: String,
-    val node_id: String,
-    val organizations_url: String,
-    val received_events_url: String,
-    val repos_url: String,
-    val site_admin: Boolean,
-    val starred_url: String,
-    val subscriptions_url: String,
-    val type: String,
-    val url: String
+    @SerializedName("avatar_url")
+    val imageProfile: String,
+    @SerializedName("login")
+    val userName: String,
 )
