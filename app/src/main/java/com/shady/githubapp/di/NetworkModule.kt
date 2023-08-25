6aff24c3615c4d52a57ad70a324e77819f6470db
Shady <ahmed.shady1@vodafone.com>
2023-08-25 12:39:57 +0300

    feat: implement Retrofit caching because API calls have a cost per call

--- app/src/main/java/com/shady/githubapp/di/NetworkModule.kt
+++ app/src/main/java/com/shady/githubapp/di/NetworkModule.kt
@@ -1,13 +1,18 @@
 package com.shady.githubapp.di
 
+import android.content.Context
 import com.shady.data.remote.ApiService
+import com.shady.githubapp.CacheInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -16,15 +21,17 @@ import javax.inject.Singleton
 object NetworkModule {
     @Provides
     @Singleton
-    fun provideOkHttp(): OkHttpClient {
+    fun provideOkHttp(@ApplicationContext appContext: Context): OkHttpClient {
         return OkHttpClient.Builder().connectTimeout(20, TimeUnit.SECONDS)
+            .cache(Cache(File(appContext.cacheDir, "http-cache"), 10L * 1024L * 1024L)) // 10 MiB
+            .addNetworkInterceptor(CacheInterceptor())
             .readTimeout(20, TimeUnit.SECONDS).build()
     }
 
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
-        return Retrofit.Builder().baseUrl("https://api.github.com/search/").client(okHttpClient)
+        return Retrofit.Builder().baseUrl(ApiService.BASE_URL).client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create()).build()
     }
 
--- data/src/main/java/com/shady/data/remote/ApiService.kt
+++ data/src/main/java/com/shady/data/remote/ApiService.kt
@@ -4,6 +4,9 @@ import com.shady.domain.entity.TrendingResponse
 import retrofit2.http.GET
 
 interface ApiService {
+    companion object {
+        const val BASE_URL = "https://api.github.com/search/"
+    }
     @GET("repositories?q=language=+sort:stars")
     suspend fun getTrending(): TrendingResponse
 }
